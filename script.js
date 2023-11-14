9fb870587742cf4aba1b1e0d032377982719085e
Kouzi99 <125484339+Kouzi99@users.noreply.github.com>
2023-11-14 17:17:29 +0100

    nice

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
 document.addEventListener("touchstart", function() {}, true);
+
+// Welcome timeout
+    setTimeout(function() {
+      document.getElementById('welcome-section').style.display = 'none';
+    }, 18000); // 2000 milliseconds (2 seconds)
