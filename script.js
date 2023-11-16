7865d5d5d6618baa7d6a26493aea28b61e5f802c
Kouzi99 <125484339+Kouzi99@users.noreply.github.com>
2023-11-16 17:24:26 +0100

    comity

--- script.js
+++ script.js
@@ -3,4 +3,28 @@ document.addEventListener("touchstart", function() {}, true);
 // Welcome timeout
     setTimeout(function() {
       document.getElementById('welcome-section').style.display = 'none';
-    }, 18000); // 2000 milliseconds (2 seconds)
+    }, 3000); // 2000 milliseconds (2 seconds)
+
+
+// on scroll animation 
+        document.addEventListener("DOMContentLoaded", function () {
+            const animatedElements = document.querySelectorAll('.animated-element');
+
+            const observer = new IntersectionObserver(entries => {
+                entries.forEach((entry, index) => {
+                    if (entry.isIntersecting) {
+                        setTimeout(() => {
+                            entry.target.classList.add('in-viewport');
+                        }, index * 200); // Adjust the delay as needed
+                    }
+                });
+            }, { threshold: 0.5 });
+
+            animatedElements.forEach(element => {
+                observer.observe(element);
+            });
+        });
+
+
+// about me
+
