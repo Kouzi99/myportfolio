58cc3ed78d112f901dcb35838791d9aac4603127
Kouzi99 <125484339+Kouzi99@users.noreply.github.com>
2023-11-28 06:58:40 +0100

    pain

--- script.js
+++ script.js
@@ -30,4 +30,64 @@ document.addEventListener("touchstart", function() {}, true);
 
 // design
 
-  
+document.addEventListener("DOMContentLoaded", function () {
+    const wavePaths = document.querySelectorAll(".st1");
+
+    wavePaths.forEach((path, index) => {
+        const length = path.getTotalLength();
+        path.style.strokeDasharray = length;
+        path.style.strokeDashoffset = length;
+        path.getBoundingClientRect();
+    });
+
+    // Call the function when the page is loaded
+    window.onload = findPathValues;
+
+    // Function to find and store path values
+    function findPathValues() {
+        // Select all path elements in the SVG
+        var pathElements = document.querySelectorAll('.wave path');
+
+        // Array to store path values
+        var pathValues = [];
+
+        // Loop through each path element and store its 'd' attribute value
+        pathElements.forEach(function (path) {
+            var pathValue = path.getAttribute('d');
+            pathValues.push(pathValue);
+        });
+
+        // Use the pathValues array for your animation or other logic
+        console.log('Path Values:', pathValues);
+
+        // Example: You can use the path values in your animation library (e.g., Anime.js)
+        // Replace this with your animation logic
+        animatePathValues(pathValues);
+    }
+
+    // Example animation function using Anime.js with a simple strokeDashoffset animation
+    function animatePathValues(pathValues) {
+        anime({
+            targets: '.wave path',
+            strokeDashoffset: [anime.setDashoffset, 0],
+            easing: 'easeInOutSine',
+            duration: 5000,
+            loop: false,
+            delay: function (el, i) {
+                return i * 2500;
+            },
+            direction: 'alternate',
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+   
